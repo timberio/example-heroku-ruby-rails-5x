86a248de164985c77ae0f0475f4e649576233269
Ben Johnson <bjohnson@binarylogic.com>
2017-03-16 23:52:45 -0500

    Add a dummy user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
+require 'ostruct'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+    def current_user
+      @current_user ||= begin
+        user = OpenStruct.new
+        user.id = 1234
+        user.first_name = "Ben"
+        user.last_name = "Johnson"
+        user.email = "hi@timber.io"
+        user
+      end
+    end
 end
