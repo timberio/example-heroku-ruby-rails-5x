3eef1d83a8e7aa853021e9585bd77fe9cbd0ec31
Ben Johnson <bjohnson@binarylogic.com>
2017-04-21 23:36:12 -0500

    Add devise and omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :products
   get '/exception', to: 'exception#index'
   root 'welcome#index'
